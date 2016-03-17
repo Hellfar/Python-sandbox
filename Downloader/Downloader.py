d748a5862674f8a098c5624931f7ff80c4d6cc96
Hellfar <pierre.louchet@gmail.com>
2016-03-17 16:02:42 +0100

    Same name files.

--- Downloader/Downloader.py
+++ Downloader/Downloader.py
@@ -10,12 +10,18 @@ if len(sys.argv) > 1:
 else:
 	work = "toDownload.txt"
 
+dirname = os.path.dirname(work)
+if len(dirname) > 0:
+	dirname += "/"
 fo = open(work, "r")
 for line in fo:
 	print "url: %s" % (line)
 	fl = requests.get(line, stream=True)
-	filename = os.path.dirname(work) + "/" + re.findall("filename=\"(\S+)\"", fl.headers['Content-Disposition'])[0]
-	os.path.isfile(filename)
+	filename = firstplace = dirname + re.findall("filename=\"(\S+)\"", fl.headers['Content-Disposition'])[0]
+	i = 0
+	while os.path.isfile(filename):
+		i += 1
+		filename = firstplace[:firstplace.rfind('.')] + " (" + str(i) + ")" + firstplace[firstplace.rfind('.'):]
 	print "in file: %s" % (filename)
 	with open (filename, "wb") as Output:
 	  for chunk in fl.iter_content(chunk_size=1024):
