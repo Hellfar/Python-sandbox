24d84855898d3b62b554fa3ab97221385d88eb66
Hellfar <pierre.louchet@gmail.com>
2016-04-21 09:52:57 +0200

    Error management.

--- Downloader/Downloader.py
+++ Downloader/Downloader.py
@@ -15,16 +15,19 @@ if len(dirname) > 0:
 	dirname += "/"
 fo = open(work, "r")
 for line in fo:
-	print "url: %s" % (line)
+	print "url: %s" % (line[:-1])
 	fl = requests.get(line, stream=True)
-	filename = firstplace = dirname + re.findall("filename=\"(\S+)\"", fl.headers['Content-Disposition'])[0]
-	i = 0
-	while os.path.isfile(filename):
-		i += 1
-		filename = firstplace[:firstplace.rfind('.')] + " (" + str(i) + ")" + firstplace[firstplace.rfind('.'):]
-	print "in file: %s" % (filename)
-	with open (filename, "wb") as Output:
-	  for chunk in fl.iter_content(chunk_size=1024):
-		if chunk:
-		  Output.write(chunk)
+	if fl:
+		filename = firstplace = dirname + re.findall("filename=\"(\S+)\"", fl.headers['Content-Disposition'])[0]
+		i = 0
+		while os.path.isfile(filename):
+			i += 1
+			filename = firstplace[:firstplace.rfind('.')] + " (" + str(i) + ")" + firstplace[firstplace.rfind('.'):]
+		print "in file: %s\n" % (filename)
+		with open (filename, "wb") as Output:
+		  for chunk in fl.iter_content(chunk_size=1024):
+			if chunk:
+				Output.write(chunk)
+	else:
+		print "Couldn't resolve this url. Ended with status(%d)\n" % fl.status_code
 fo.close()
